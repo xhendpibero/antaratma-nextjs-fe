2d89948661978d7c20caa4509254b88e3c662f7d
Dendy Sapto A <44661300+xhendpibero@users.noreply.github.com>
2023-06-27 09:18:37 +0700

    test

---
+++ src/app/artikel/add/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import ListArtikel from '@/layouts/Artikel/ListArtikel';
+
+export default function ArtikelPage() {
+  return <ListArtikel />;
+}
---
+++ src/app/user/booking/[id]/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import User from '@/layouts/User/User';
+
+export default function AboutPage() {
+  return <User />;
+}
